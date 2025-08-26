1d95e59556bee930d14b75219e296238d6376408
Luis <felipenarciso15@gmail.com>
2025-08-25 21:20:14 -0300

    criação do user data builder

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,18 +1,15 @@
+import { UserDataBuilder } from '../../../testing/helpers/user-data-builder';
 import { UserEntity, UserProps } from '../../user.entity';
-import { faker } from '@faker-js/faker';
+
 
 describe('UserEntity unifn: t tests', () => {
   let props: UserProps;
   let sut: UserEntity;
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
-
+    props = UserDataBuilder();
     sut = new UserEntity(props);
   });
+
   it('Constructor method', () => {
     expect(sut.props.name).toEqual(props.name);
     expect(sut.props.email).toEqual(props.email);
